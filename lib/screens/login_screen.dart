444312073369387631a98f994f130a0fdb60ffbe
Lourdes Ruiz <ruizsantiago96@gmail.com>
2026-09-08 21:54:14 -0600

    feat(rive): Animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //para obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding:EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('osito.riv'),
+              ),
+            ]
+          ),
+        ) , // Padding
+      ), //SafeArea
+    );
+  }
+}
